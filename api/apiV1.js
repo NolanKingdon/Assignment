f1799f66236f1370af3913bc87ddc5b490c19a15
Nolan Kingdon <nolan.kingdon@gmail.com>
2020-12-06 11:32:17 -0500

    Added APIV1 router to handle calls to backend

---
+++ api/apiV1.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const apiV1 = express.Router();
+const luhn = require('luhn');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+apiV1.post('/purchase', jsonParser, (req, res) => {
+    let valid = luhn.validate(req.body.cardNumber);
+
+    if (valid) {
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+module.exports = apiV1;
